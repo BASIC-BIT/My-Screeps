fc435d1839b6838e58320110c4e37786e2b448a6
Steven Kneisler <stevenkneisler@gmail.com>
2018-04-26 00:00:48 -0400

    Make towers heal creeps

--- src/prototype.tower.js
+++ src/prototype.tower.js
@@ -2,11 +2,19 @@
 StructureTower.prototype.defend = function () {
   if (this.pos) {
     // find closes hostile creep
-    const target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    let target = this.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
     // if one is found...
     if (target) {
       // ...FIRE!
       this.attack(target);
+    }
+
+    target = this.pos.findClosestByRange(FIND_MY_CREEPS, {
+      filter: creep => creep.hits < creep.hitsMax,
+    });
+
+    if (target) {
+      this.heal(target);
     }
   }
 };
