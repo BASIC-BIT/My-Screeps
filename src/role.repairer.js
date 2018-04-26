b9dac88b20331c07f3021cad3ef25456a58f5c4d
Steven Kneisler <stevenkneisler@gmail.com>
2018-04-26 00:06:04 -0400

    Make repairers give energy to towers

--- src/role.repairer.js
+++ src/role.repairer.js
@@ -18,12 +18,21 @@ module.exports = {
       // find closest structure with less than max hits
       // Exclude walls because they have way too many max hits and would keep
       // our repairers busy forever. We have to find a solution for that later.
-      const structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+      let structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         // the second argument for findClosestByPath is an object which takes
         // a property called filter which can be a function
         // we use the arrow operator to define it
         filter: s => s.hits < s.hitsMax && s.structureType !== STRUCTURE_WALL,
       });
+
+      if (structure === undefined) {
+        structure = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+          // the second argument for findClosestByPath is an object which takes
+          // a property called filter which can be a function
+          // we use the arrow operator to define it
+          filter: s => s.structureType === STRUCTURE_TOWER && s.energy < s.energyCapacity,
+        });
+      }
 
       // if we find one
       if (structure) {
@@ -32,7 +41,9 @@ module.exports = {
           // move towards it
           creep.moveTo(structure);
         }
-      } else {
+      }
+
+      if (structure === undefined) {
         // look for construction sites
         roleBuilder.run(creep);
       }
